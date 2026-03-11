3d49ff736259de026cb419d07ffdf829e9594bb4
Nacho <nacho@molinacobo.com>
2026-03-11 19:46:20 +0100

    Eliminar nombre de fichero de salida opcional

--- scripts/medicion.sh
+++ scripts/medicion.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 # script genérico para medir un ejecutable sobre un rango de tamaños de entrada
 # autor: Juan Ignacio Molina
-# uso: medicion.sh <programa> <inicio> <fin> <incremento> [archivo-salida]
+# uso: medicion.sh <programa> <inicio> <fin> <incremento>
 
 
 # exigir al menos el programa y parámetros de medicion
 if [ $# -lt 4 ]; then
-    echo "Uso: $0 <programa> <inicio> <fin> <incremento> [archivo-salida]"
+    echo "Uso: $0 <programa> <inicio> <fin> <incremento>"
     exit 1
 fi
 
@@ -19,17 +19,15 @@ inicio=$2
 fin=$3
 incremento=$4
 tam_semilla=42
-
-# archivo de salida ejecutable.dat o el indicado
-if [ $# -ge 5 ]; then
-    salida=$5
-else
-    salida=".$ROOT_DIR/data/$(basename $ejecutable).dat"
-fi
+salida=".$ROOT_DIR/data/$(basename $ejecutable).dat"
 
 echo "Escribiendo en $salida..."
 
 i=$inicio
+
+# asegurar que existe el directorio de salida
+mkdir -p $(dirname $salida)
+
 # asegurar que el archivo de salida existe para que el append funcione
 echo "" > $salida
 while [ $i -lt $fin ]; do
