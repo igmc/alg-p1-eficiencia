c918403a37d5fac6310fdf5b1931d563cee2adc6
Nacho <nacho@molinacobo.com>
2026-03-11 18:13:46 +0100

    Añadida implementación de Fibonacci

---
+++ src/fibonacci.cpp
@@ -0,0 +1,40 @@
+// compilar con g++ -std=c++11 ./src/fibonacci.cpp -o ./bin/fibonacci
+// Implementacin del algoritmo de Fibonacci
+// Autor: Juan Ignacio Molina Cobo
+
+#include <cstdlib>
+#include <chrono>
+#include <iostream>
+using namespace std;
+
+unsigned int Fibonacci(unsigned int n){
+    if (n == 0) return 0;
+    else if (n == 1) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        cerr << "Uso: " << argv[0] << " <semilla> <n>\n";
+        return EXIT_FAILURE;
+    }
+
+    unsigned long int semilla;
+    semilla = atoi(argv[1]);
+    srand(semilla); // Inicializamos generador de no. aleatorios
+
+    int n = atoi(argv[2]);
+
+    std::chrono::high_resolution_clock::time_point t0, tf;
+    std::chrono::duration<double> duracion;
+
+    t0 = std::chrono::high_resolution_clock::now();
+    unsigned int result = Fibonacci(n);
+    tf = std::chrono::high_resolution_clock::now();
+
+    duracion = std::chrono::duration_cast<std::chrono::duration<double>>(tf - t0);
+    
+    cout << n << " " << duracion.count() << endl;
+
+    return EXIT_SUCCESS;
+}
