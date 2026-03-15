c81c9b81a6c7f9383f86568bcc5991dc4808f1f6
Nacho <nacho@molinacobo.com>
2026-03-15 16:22:56 +0100

    Añadir script para hacer todas las graficas

---
+++ scripts/graficar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# script para lanzar gnuplot para graficar los resultados de las mediciones
+# autor: Juan Ignacio Molina
+# uso: graficar.sh
+
+script_dir=$(dirname $0)
+data_dir="$script_dir/../data"
+output_dir="$script_dir/../figures"
+
+# asegurar que existe el directorio de salida
+mkdir -p $output_dir
+
+# lanzar gnuplot
+gnuplot "$script_dir/exponenciales.gp"
+gnuplot "$script_dir/cuadraticos.gp"
+gnuplot "$script_dir/logaritmicos.gp"
